da7f86188bb9232fe4252f0932538d6320462ec5
Mayank Tripathi <mayankchamps27@gmail.com>
2024-10-27 21:31:44 +0530

    prepare base model added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Kidney_Disease_Classification import logger
 from Kidney_Disease_Classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Kidney_Disease_Classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -15,3 +16,14 @@ except Exception as e:
     # not caught at a higher level. This allows the exception to propagate up the call stack for
     # further handling or debugging.
     raise e
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
